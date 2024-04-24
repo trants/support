bf63aced0c65f08bf1d9ac07d0ddc7c70634c054
Son Tran Thanh <286.trants@gmail.com>
2024-04-24 19:26:25 +0700

    Add MailerTraitTest

---
+++ tests/Traits/MailerTraitTest.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * *************************************************************************
+ * Copyright (c) VSP Co., Ltd - All Rights Reserved
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ */
+
+namespace Vspc\Support\Tests\Traits;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Vspc\Support\Traits\MailerTrait;
+
+class MailerTraitTest extends TestCase
+{
+    /**
+     * @inheritdoc
+     */
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_set_and_retrieve_the_mailer_instance()
+    {
+        $mailerTrait = new MailerTraitStub();
+
+        $mailer = m::mock('Vspc\Support\Mailer');
+
+        $mailerTrait->setMailer($mailer);
+
+        $this->assertSame($mailerTrait->getMailer(), $mailer);
+    }
+}
+
+class MailerTraitStub
+{
+    use MailerTrait;
+}
